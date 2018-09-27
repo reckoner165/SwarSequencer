94359fcb05dc29f0ea561d1fe1e2bf3e38b60e1f
Sumanth Srinivasan <sumanth@vimeo.com>
2018-09-27 15:16:29 -0400

    Setup multithreads and queuing for livecoding

--- repl.py
+++ repl.py
@@ -7,6 +7,9 @@ from util.freq import get_frequency
 import code
 import os
 
+from threading import Thread
+import Queue
+
 options = {
     'format': paInt16,
     'channels': 2,
@@ -18,34 +21,58 @@ root = [440]
 
 patcher = Patcher(options)
 module = patcher.module
-T = 0.3
+T = 0.2
+notes_queue = Queue.Queue()
+
+
+def sequence(queue):
+    print "playing notes!"
+    old_notes = [1]
+    while True:
+
+        try:
+            new_notes = queue.get(timeout=0.1)
+            old_notes = new_notes[:]
+        except Queue.Empty:
+            pass
+
+        for note in old_notes:
+            freq = get_frequency(root[0], int(note) - 1)
+            osc = module.osc_tone(T, freq)
+            patcher.to_master(osc, 0.5, 0.5)
+
+
+t = Thread(target=sequence, args=(notes_queue,))
+t.daemon = True
+t.start()
+
 
 def play_aaroha(raag_name):
     raag = get_raag(raag_name)
-    for note in raag['aaroha']:
-        freq = get_frequency(root[0], int(note) - 1)
-        osc = module.osc_tone(T, freq)
-        patcher.to_master(osc, 0.5, 0.5)
+    notes = raag['aaroha']
+    notes_queue.put(notes)
 
 def play_avaroha(raag_name):
     raag = get_raag(raag_name)
-    for note in raag['avaroha']:
-        freq = get_frequency(root[0], int(note) - 1)
-        osc = module.osc_tone(T, freq)
-        patcher.to_master(osc, 0.5, 0.5)
+    notes = raag['avaroha']
+    notes_queue.put(notes)
 
 def repl_exit():
     patcher.terminate()
     exit()
 
+
 def get_raags():
     print get_raag_list()
 
+
 def honk(message):
     os.system('say "' + message + '"')
+
 
 if __name__ == "__main__":
     code.interact(
         banner="Now interactively running SwarSequencer",
         local=locals()
     )
+

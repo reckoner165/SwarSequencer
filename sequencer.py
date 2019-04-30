7a4deafff47f1e41336cbb364081136e078effdb
Sumanth Srinivasan <sumanth.srinivasan@gmail.com>
2019-04-30 18:44:48 -0400

    Move sequencer to separate class

--- sequencer.py
+++ sequencer.py
@@ -1,31 +1,49 @@
 __author__ = 'Sumanth Srinivasan'
 
 from pyaudio import paInt16
-
 from packages.soundmodular import Patcher
-from noteparser import get_raag
 from util.freq import get_frequency
+import Queue
 
-options = {
-    'format': paInt16,
-    'channels': 2,
-    'sampling_rate': 22000,
-    'save_file': 'testfile.wav'
-}
+class Sequencer:
+    def __init__(self):
+        options = {
+            'format': paInt16,
+            'channels': 2,
+            'sampling_rate': 22000,
+            'save_file': 'testfile.wav'
+        }
 
-root = [440]
-raag_name = ['Adana']
-raag = get_raag(raag_name[0])
-print raag['aaroha']
+        self.patcher = Patcher(options)
+        self.module = self.patcher.module
 
-patcher = Patcher(options)
-module = patcher.module
-T = 0.3                 # Time in seconds
+    def sequence(self, queue):
+        current_status = {
+            'notes': [1],
+            'root': [440],
+            'duration': [0.2],
+            'stutter': [1]
+        }
 
-for note in raag['aaroha']:
-    # print (type(note))
-    freq = get_frequency(root[0], int(note) - 1)
-    osc = module.osc_tone(T, freq)
-    patcher.to_master(osc, 0.5, 0.5)
+        while True:
 
-patcher.terminate()
+            try:
+                new_status = queue.get(timeout=0.1)
+                current_status = dict(current_status, **new_status)
+            except Queue.Empty:
+                pass
+
+            '''
+            TODO: for note, time in zip(current_status['notes'], current_status['time'])
+            '''
+
+            for note in current_status['notes']:
+                if note == 0:
+                    continue
+
+                freq = get_frequency(current_status['root'][0], int(note) - 1)
+                for i in range (0, current_status['stutter'][0]):
+                    osc = self.module.osc_tone(current_status['duration'][0] / current_status['stutter'][0], freq)
+                    self.patcher.to_master(osc, 0.5, 0.5)
+
+
